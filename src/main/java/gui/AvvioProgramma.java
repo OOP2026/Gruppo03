2c9f461fcbfbbeff920beaed15ae5ff5cb71f72e
ariiovine <ari.iovine@studenti.unina.it>
2026-05-26 02:16:59 +0200

    sistemati i commenti nella classe avvio programma

--- src/main/java/gui/AvvioProgramma.java
+++ src/main/java/gui/AvvioProgramma.java
@@ -2,6 +2,7 @@ package gui;
 
 import controller.Controller;
 
+// classe principale che fa partire tutto il sistema ospedaliero
 public class AvvioProgramma {
     public static void main(String[] args) {
         Controller controller = new Controller();
