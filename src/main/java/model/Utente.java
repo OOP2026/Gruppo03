a7be9fbbe8f10c9d329f9f26b7517b139454b366
ariiovine <ari.iovine@studenti.unina.it>
2026-05-26 02:12:35 +0200

    sistemati i commenti nella classe utente

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,19 +1,21 @@
 package model;
-
+// rappresenta un utente generico del sistema con credenziali di accesso
 public class Utente {
     private String login;
     private String password;
 
+    // costruttore per inizializzare le credenziali dell'utente
     public Utente(String login, String password) {
         this.login = login;
         this.password = password;
     }
 
+    // verifica se le credenziali inserite corrispondono a quelle salvate
     public boolean login(String login, String password) {
         return this.login.equals(login) && this.password.equals(password);
     }
 
-    // getter e setter
+    // metodi getter e setter per login e password
     public String getLogin() {
         return login;
     }
